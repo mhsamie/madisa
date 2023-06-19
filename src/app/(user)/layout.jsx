99a0a618cf9a1b1e9ae3e13081cf758785b89aea
mhsamie <sfatima7720@gmail.com>
2023-06-19 11:40:31 +0330

    fundamental chnages

---
+++ src/app/(user)/layout.jsx
@@ -0,0 +1,27 @@
+import vazirFont from "@/constants/localFonts";
+import "../globals.css";
+import Header from "../Header";
+import { Toaster } from "react-hot-toast";
+import Providers from "../Providers";
+
+export const metadata = {
+  title: "Next Shop Panel",
+  description: "Next.js Course Fronthooks Course",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="fa" dir="rtl">
+      <body
+        suppressHydrationWarning={true}
+        className={`${vazirFont.variable} font-sans`}
+      >
+        <Providers>
+          <Toaster />
+          <Header />
+          <div className="container xl:max-w-screen-xl">{children}</div>
+        </Providers>
+      </body>
+    </html>
+  );
+}
